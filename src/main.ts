69a31f4113d18c8221258f8b45023015bb7e8343
David Arellano <david.arellano@octopy.com>
2021-03-11 11:00:52 -0600

    Incrmento de timeout al server

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,40 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
-import { PORT, IS_DEVELOPMENT, KEY_FILE, CERT_FILE, API_PREFIX } from './config';
+import {
+  PORT,
+  IS_DEVELOPMENT,
+  KEY_FILE,
+  CERT_FILE,
+  API_PREFIX,
+} from './config';
 
 async function bootstrap() {
-  
   let app;
-  if(IS_DEVELOPMENT){
-    app = await NestFactory.create(AppModule);    
-  }else{
+  if (IS_DEVELOPMENT) {
+    app = await NestFactory.create(AppModule);
+  } else {
     const httpsOptions = {
       key: fs.readFileSync(KEY_FILE),
-      cert: fs.readFileSync(CERT_FILE)
-    }
-    app = await NestFactory.create(AppModule,{httpsOptions});
+      cert: fs.readFileSync(CERT_FILE),
+    };
+    app = await NestFactory.create(AppModule, { httpsOptions });
   }
-  
-  app.setGlobalPrefix(API_PREFIX)
-  app.enableCors()
+
+  app.setGlobalPrefix(API_PREFIX);
+  app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle("NOVEVE API's").setDescription("Press the schema option found at the top of each example to see more detail")
+    .setTitle("NOVEVE API's")
+    .setDescription(
+      'Press the schema option found at the top of each example to see more detail',
+    )
     .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(`/${API_PREFIX}/api`, app, document);
-  
-  await app.listen(PORT);
+
+  const server = await app.listen(PORT);
+  server.setTimeout(60000);
 }
 bootstrap();
