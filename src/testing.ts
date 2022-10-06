57e553ce273cd5994f24d31419fc174bfb4692b5
Bjorler <gth86@hotmail.com>
2022-10-06 14:56:56 -0500

    [update] configs

---
+++ src/testing.ts
@@ -0,0 +1,11 @@
+
+export const HOST = "localhost";
+export const USER = "root";
+export const PASSWORD = "";
+export const DATABASE = "noveve";
+export const SECRET = "0c7Op12o2L$n0v3vE"
+export const METHOD = "http";
+export const DOMAIN = "localhost";
+export const PORT = 4057;
+export const  USER_EMAIL = "";
+export const PASSWORD_EMAIL = "";
